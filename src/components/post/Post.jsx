85c76d3047464f1db8927e495fddc45533d3bc90
hieutran018 <73825993+hieutran018@users.noreply.github.com>
2023-03-18 17:26:58 +0700

    show dialogPostDetail

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { MoreVert } from "@mui/icons-material";
 import PublicIcon from '@mui/icons-material/Public';
 import GroupIcon from '@mui/icons-material/Group';
 import LockIcon from '@mui/icons-material/Lock';
@@ -8,12 +7,9 @@ import ImageListItem from '@mui/material/ImageListItem';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import ShowMoreText from "react-show-more-text";
 import moment from 'moment';
-
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
 
 import 'moment/locale/vi';
 // import { useState } from "react";
@@ -55,9 +51,6 @@ function Post({ post }) {
                             {post.username}
                         </span>
 
-                    </div>
-                    <div className="postTopRight">
-                        <MoreVert />
                     </div>
                 </div>
                 <div className="postPrivacy">
@@ -119,41 +112,132 @@ function Post({ post }) {
                                     ))}
                                 </ImageList>}
                 </div>
+                <div className="postBottomStatistical">
+                    <span className="postTextStatistical">20k lượt thích</span>
+                    <div>
+                        <span className="postTextStatistical statisticalComment">20k bình luận</span>
+                        <span className="postTextStatistical">20k lượt chia sẻ</span>
+                    </div>
+                </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-
-                        <div className="postBottomButton"><button onClick={handleClickOpen} className="btn ">Thích </button></div>
-                        <div className="postBottomButton"><button className="btn ">Bình luận</button></div>
+                        <div className="postBottomButton"><button className="btn ">Thích </button></div>
+                        <div className="postBottomButton"><button onClick={handleClickOpen} className="btn ">Bình luận</button></div>
                         <div className="postBottomButton"><button className="btn ">Chia sẻ</button></div>
                         {/* <span className="postLikeCounter">{like}</span> */}
                     </div>
-                    {/* <div className="postBottomRight">
-                        <span className="postCommentText">{post.comment} bình luận</span>
-                    </div> */}
+
                 </div>
             </div>
             <div>
                 <Dialog
                     open={open}
                     onClose={handleClose}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
+                    fullWidth
+                    maxWidth="md"
                 >
-                    <DialogTitle id="alert-dialog-title">
-                        {"Use Google's location service?"}
-                    </DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            Let Google help apps determine location. This means sending anonymous
-                            location data to Google, even when no apps are running.
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <button onClick={handleClose}>Disagree</button>
-                        <button onClick={handleClose} autoFocus>
-                            Agree
-                        </button>
-                    </DialogActions>
+                    <div className="dialogPost">
+                        <div>
+                            <div className="postTitle">
+                                <div style={{ width: "10%" }}></div>
+                                <div>
+                                    <span className="postTitleDialog">Bài viết của {post.username}</span>
+                                </div>
+                                <div className="dialoPostButtonClose" style={{ width: "10%" }}>
+                                    <IconButton className="buttonClose" aria-label="delete" size="large">
+                                        <ClearIcon fontSize="inherit" />
+                                    </IconButton>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="postWrapper">
+                            <div className="postTop">
+                                <div className="postTopLeft">
+                                    <img
+                                        className="postProfileImg"
+                                        src={post.avatarUser}
+                                        alt=""
+                                    />
+                                    <span className="postUsername">
+                                        {post.username}
+                                    </span>
+
+                                </div>
+
+                            </div>
+                            <div className="postPrivacy">
+                                <span className="postDate">{moment(post.created_at, 'YYYYMMDD h:mm:ss').fromNow()}
+                                    {post.privacy === 0 ? <LockIcon className="postIconPrivacy" /> : post.privacy === 1 ? <PublicIcon className="postIconPrivacy" /> : post.privacy === 2 ? <GroupIcon className="postIconPrivacy" /> : <PersonRemoveIcon className="postIconPrivacy" />}</span>
+                            </div>
+                            <div className="postCenter">
+                                <ShowMoreText
+                                    /* Default options */
+                                    lines={3}
+                                    more="xem thêm"
+                                    less="ẩn bớt"
+                                    className="postText"
+                                    anchorClass="postViewMore"
+                                    onClick={executeOnClick}
+                                    expanded={false}
+                                    truncatedEndingComponent={"... "}
+                                >{post.post_content}
+                                </ShowMoreText>
+                                {post.totalMediaFile === 1 ?
+                                    <div>
+                                        <img className="postImg" src={post.mediafile[0].media_file_name} alt="" />
+                                    </div>
+                                    : post.totalMediaFile === 2 ?
+                                        <ImageList sx={{ width: "100%", height: "100%" }} cols={2} rowHeight={350}>
+                                            {post.mediafile.map((item) => (
+                                                <ImageListItem key={item.media_file_name}>
+                                                    <img
+                                                        src={`${item.media_file_name}?w=164&h=164&fit=crop&auto=format`}
+                                                        srcSet={`${item.id}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                                        alt={item.title}
+                                                        loading="lazy"
+                                                    />
+                                                </ImageListItem>
+                                            ))}
+                                        </ImageList> : post.totalMediaFile === 3 ?
+                                            <ImageList sx={{ width: "100%", height: "100%" }} cols={3} rowHeight={300}>
+                                                {post.mediafile.map((item) => (
+                                                    <ImageListItem key={item.media_file_name}>
+                                                        <img
+                                                            src={`${item.media_file_name}?w=164&h=164&fit=crop&auto=format`}
+                                                            srcSet={`${item.id}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                                            alt={item.title}
+                                                            loading="lazy"
+                                                        />
+                                                    </ImageListItem>
+                                                ))}
+                                            </ImageList> :
+                                            <ImageList sx={{ width: "100%", height: "100%" }} cols={2} rowHeight={350}>
+                                                {post.mediafile.map((item) => (
+                                                    <ImageListItem key={item.media_file_name}>
+                                                        <img
+                                                            src={`${item.media_file_name}?w=164&h=164&fit=crop&auto=format`}
+                                                            srcSet={`${item.id}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                                            alt={item.title}
+                                                            loading="lazy"
+                                                        />
+                                                    </ImageListItem>
+                                                ))}
+                                            </ImageList>}
+                            </div>
+                            <div className="postBottomStatistical">
+                                <span className="postTextStatistical">20k lượt thích</span> <div><span className="postTextStatistical statisticalComment">20k bình luận</span><span className="postTextStatistical">20k lượt chia sẻ</span></div>
+                            </div>
+                            <div className="postBottom">
+                                <div className="postBottomLeft">
+                                    <div className="postBottomButton"><button className="btn ">Thích </button></div>
+                                    <div className="postBottomButton"><button onClick={handleClickOpen} className="btn ">Bình luận</button></div>
+                                    <div className="postBottomButton"><button className="btn ">Chia sẻ</button></div>
+                                    {/* <span className="postLikeCounter">{like}</span> */}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </Dialog>
             </div>
         </div>
